d6a2261d372c9f0598748d853b14f903f4a1c218
TerrenceSiau0304 <terrencesiau0304@gmail.com>
2026-08-12 01:02:42 +0800

    Turn function to constant to avoid creating TavilySearch object for each call

--- backend/rag/web_search.py
+++ backend/rag/web_search.py
@@ -3,5 +3,7 @@ from langchain_tavily import TavilySearch
 
 from core.config import get_settings
 
-def web_search_tool():
-    return TavilySearch(tavily_api_key = get_settings().tavily_api_key)
+web_search_tool = TavilySearch(
+    max_results=5,
+    tavily_api_key=get_settings().tavily_api_key,
+)
