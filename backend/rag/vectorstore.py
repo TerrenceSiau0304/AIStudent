dd0c1f7990d5b967c50eb103cb4fb5e9c799c164
TerrenceSiau0304 <terrencesiau0304@gmail.com>
2026-08-16 00:41:59 +0800

    Revert vectorstore class

--- backend/rag/vectorstore.py
+++ backend/rag/vectorstore.py
@@ -2,26 +2,24 @@ import uuid
 from pathlib import Path
 from langchain_core.documents import Document
 from langchain_core.stores import InMemoryByteStore
-# from langchain_classic.storage import LocalFileStore
-# from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_classic.storage import LocalFileStore
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_classic.retrievers.multi_vector import MultiVectorRetriever
 
-# from core.config import DATA_DIR
+from core.config import DATA_DIR
 
 #pip install langchain-huggingface removed from requirement.txt for space, (HuggingFaceEmbeddings(model_name="BAAI/bge-base-en-v1.5"))
 
 
 
 class VectorStore:
-    def __init__(self, collection_name: str, embedding_function=None):
+    def __init__(self, collection_name: str, embedding_function=(HuggingFaceEmbeddings(model_name="BAAI/bge-base-en-v1.5"))):
         self.retriever = MultiVectorRetriever(
             vectorstore=Chroma(collection_name=collection_name,
                                 embedding_function=embedding_function,
-                                # persist_directory=str(DATA_DIR/"chroma_persist")
-                                persist_directory=str(Path("/tmp")/"chroma_persist")),
-            # byte_store=LocalFileStore(str(DATA_DIR/"docstore")),
-            byte_store= InMemoryByteStore(),
+                                persist_directory=str(DATA_DIR/"chroma_persist")),
+            byte_store=LocalFileStore(str(DATA_DIR/"docstore")),
             id_key="doc_id"
         )
 
